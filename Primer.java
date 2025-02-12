afad0a5429bf67678e9c9d9c92c4a30c9eb6b6c1
peter-savron <peter.savron03@gmail.com>
2025-02-12 10:35:14 +0100

    dodatki

--- Primer.java
+++ Primer.java
@@ -5,38 +5,41 @@ public class Primer {
         
         //TEGA NE TIKAJ
         Scanner sc = new Scanner(System.in);
+        //TEGA ZGORAJ NE TIKAJ
 
         //to lahko kopiras in ponovis, ce zelis vstaviti vec spremenljivk
         // POZOR: zamenjati mores ime spremenljivke
-
+        
+        System.out.print("a = ");
         int a = sc.nextInt();
+        System.out.print("b = ");
         int b = sc.nextInt();
 
         //stevil lahko primerjamo z < > <= >= == in !=
         //dobimo rezultat ki je true (DRZI) ali false (NE DRZI)
-        System.out.println(a<b);
-        System.out.println(a==b);
+        System.out.println("a < b je res? " + (a<b));
+        System.out.println("a = b je res? " + (a==b));
         
         //dolocene stvari lahko izvedemo samo ce nekaj drzi
         if(a < b){
             System.out.println("a je manjsi od b");
         }else{
-            System.out.println("s je vecji ali enak kot b");
+            System.out.println("a je vecji ali enak kot b");
         }
 
         //vrednosti stevil lahko spreminjamo
-        System.out.println("a = " + a);
+        System.out.println("Na zacetku a = " + a);
         a = a + 5;
         //vrednost a se je poveòala za 5
-        System.out.println("a = " + a);
+        System.out.println("Smo dodali 5, a = " + a);
         a = a / 2;
         //vrednost a s e je delila z 2
-        System.out.println("a = " + a);
+        System.out.println("Smo delili z 2, a = " + a);
 
         //stvari lahko ponavljamo veckrat, lahko pa jih napisemo samo enkrat:
         int i = 0;
         while(i < 10){
-            System.out.println("i = " + i);
+            System.out.println("Zdaj je i = " + i);
             i = i + 1;
         }
         
---
+++ english-terms.md
@@ -0,0 +1,30 @@
+# Vocabulary of Slovenian math terms in English
+## Splošni izrazi - Common terms
+* število - number
+* seštevati, seštej - add
+* vsota - sum
+* odštevati, odštej - substract
+* razlika - difference
+* množiti, pomnoži, zmnoži - multiply
+* produkt - product
+* deliti, deli - divide
+* količnik - quotient
+* ostanek - remainder
+* primerjati, primerjaj - compare
+## Razcep na prafaktorje - Prime factorization
+* praštevilo - prime number
+* razcep - factorization
+* razcep na prafaktorje - prime factorization
+## Delitelji in večkratniki - Divisors and multiples
+* delitelj - divisor
+* je deljiv - is divisible
+* največji skupni delitelj - greatest common divisor
+* večkratnik - multiple
+* najmanjši skupni večkratnik - least common multiplier
+## Ulomki - fractions
+* ulomek - fraction
+* števec - enumerator (upper part)
+* imenovalec - denominator (lower part)
+* razširi ulomek - expand the fraction
+* okrajšaj ulomek - simplify the fraction
+* najmanjši skupni imenovalec - least common denominator
